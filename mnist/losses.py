ea57d1fb9b4c5286fabfb8c34589cbf76007edf9
SuciuCaesar12 <114161038+SuciuCaesar12@users.noreply.github.com>
2024-10-21 09:43:26 +0200

    Added loss functions

---
+++ mnist/losses.py
@@ -0,0 +1,59 @@
+import torch
+
+
+class BaseLoss:
+    def __init__(self, name=""):
+        self.name = name 
+        self.losses = []
+
+    def __call__(self, *args, **kwargs):
+        value = self._calculate_loss(*args, **kwargs)
+        self.losses.append(value.item())
+        return value
+
+    def result(self, reset: bool = True):
+        mean_loss = sum(self.losses) / len(self.losses) if self.losses else None
+        if reset:
+            self.reset()
+            
+        return mean_loss
+
+    def reset(self):
+        self.losses = []
+
+    def _calculate_loss(self, *args, **kwargs):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class LossDiscDataMI(BaseLoss):
+    def __init__(self, name="disc_data_mi"):
+        super().__init__(name)
+    
+    def _calculate_loss(self, d_prob, m):
+        return -torch.mean(d_prob * m) + torch.mean((1 - m) * d_prob)
+
+
+class LossGenMI(BaseLoss):
+    def __init__(self, name="loss_gen_mi"):
+        super().__init__(name)
+    
+    def _calculate_loss(self, d_prob, m):
+        return -torch.mean(d_prob * (1 - m))
+
+
+class LossReconstruct(BaseLoss):
+    def __init__(self, name="loss_recon"):
+        super().__init__(name)
+        
+    def _calculate_loss(self, x, x_hat, m):
+        return ((1 - m) * ((x - x_hat) ** 2)).sum() / ((1 - m).sum() + 1e-8)
+
+
+class LossStages:
+    missing_data_imputation = (LossDiscDataMI(), LossGenMI(), LossReconstruct())
+
+    @classmethod
+    def get(cls):
+        return {
+            'missing_data_imputation': cls.missing_data_imputation
+        }
